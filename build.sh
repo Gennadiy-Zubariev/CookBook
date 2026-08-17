b85369b379fd6c4ec1b7384dbfc405f7d9f8efde
Gena <genada89@gmail.com>
2026-08-17 22:59:33 +0300

    reset built.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+bash
 #!/usr/bin/env bash
 set -o errexit
 
@@ -6,7 +7,19 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.core.management import call_command
-call_command('seed_db', flush=True)
-print('Re-seeded with Cloudinary!')
+import os
+print('CLOUD_NAME:', os.getenv('CLOUDINARY_CLOUD_NAME'))
+print('API_KEY:', os.getenv('CLOUDINARY_API_KEY', 'NOT SET'))
+from django.conf import settings
+print('STORAGE:', settings.DEFAULT_FILE_STORAGE)
+"
+
+python manage.py shell -c "
+from dishes.models import Recipe
+if not Recipe.objects.exists():
+    from django.core.management import call_command
+    call_command('seed_db')
+    print('Database seeded!')
+else:
+    print('Skipping seed - database has data')
 "
