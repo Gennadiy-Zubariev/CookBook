ce35f526f14902b4dd40906141d54b3207faf1ec
Gena <genada89@gmail.com>
2026-08-17 15:18:57 +0300

    adjusting image rendering in prod config

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookbook.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookbook.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
