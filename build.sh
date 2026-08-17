326ebdfa53422c213cd9182bdd97b31173d0f66e
Gena <genada89@gmail.com>
2026-08-17 23:15:37 +0300

    reset built.sh

--- build.sh
+++ build.sh
@@ -5,38 +5,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-
-python manage.py shell -c "
-from django.conf import settings
-print('DEFAULT_FILE_STORAGE:', settings.DEFAULT_FILE_STORAGE)
-print('CLOUDINARY_STORAGE:', settings.CLOUDINARY_STORAGE)
-print('MEDIA_URL:', settings.MEDIA_URL)
-
-# Test Cloudinary connection
-import cloudinary
-cloudinary.config(
-    cloud_name=settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
-    api_key=settings.CLOUDINARY_STORAGE['API_KEY'],
-    api_secret=settings.CLOUDINARY_STORAGE['API_SECRET'],
-)
-import cloudinary.api
-result = cloudinary.api.ping()
-print('Cloudinary ping:', result)
-"
-
-
 python manage.py shell -c "
 from django.core.management import call_command
 call_command('seed_db', flush=True)
-print('Re-seeded with Cloudinary!')
-"
 
-#python manage.py shell -c "
-#from dishes.models import Recipe
-#if not Recipe.objects.exists():
-#    from django.core.management import call_command
-#    call_command('seed_db')
-#    print('Database seeded!')
-#else:
-#    print('Skipping seed - database has data')
-#"
+# Перевірка що Cloudinary працює
+from dishes.models import Recipe
+r = Recipe.objects.first()
+if r and r.image:
+    print('Image URL:', r.image.url)
+    print('Image name:', r.image.name)
+"
