a6d6cf0c24d351a3e4e47ab07ad0945597cadbc4
Gena <genada89@gmail.com>
2026-08-17 22:53:09 +0300

    test adding media

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
+import os
+print('CLOUD_NAME:', os.getenv('CLOUDINARY_CLOUD_NAME'))
+print('API_KEY:', os.getenv('CLOUDINARY_API_KEY', 'NOT SET'))
+from django.conf import settings
+print('STORAGE:', settings.DEFAULT_FILE_STORAGE)
+"
+
+python manage.py shell -c "
 from dishes.models import Recipe
 if not Recipe.objects.exists():
     from django.core.management import call_command
