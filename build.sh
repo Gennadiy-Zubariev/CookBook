346ad297e102f0ec3ebace0247ac473fdd47670b
Gena <genada89@gmail.com>
2026-08-17 21:47:33 +0300

    add seed_db to build.sh, change seed_db to more realistic data

--- build.sh
+++ build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Seed only if database is empty
+python manage.py shell -c "
+from dishes.models import Recipe
+if not Recipe.objects.exists():
+    from django.core.management import call_command
+    call_command('seed_db')
+    print('Database seeded!')
+else:
+    print('Skipping seed - database has data')
+"
