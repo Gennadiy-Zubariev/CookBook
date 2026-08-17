4e455d718e337d58a716d3f3dd7caa5f4d51c94f
Gena <genada89@gmail.com>
2026-08-17 23:28:27 +0300

    return built with check if db is not empty

--- build.sh
+++ build.sh
@@ -6,21 +6,11 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.core.management import call_command
-call_command('seed_db', flush=True)
-
 from dishes.models import Recipe
-r = Recipe.objects.first()
-if r and r.image:
-    print('Image URL:', r.image.url)
+if not Recipe.objects.exists():
+    from django.core.management import call_command
+    call_command('seed_db')
+    print('Database seeded!')
+else:
+    print('Skipping seed - database has data')
 "
-
-#python manage.py shell -c "
-#from dishes.models import Recipe
-#if not Recipe.objects.exists():
-#    from django.core.management import call_command
-#    call_command('seed_db')
-#    print('Database seeded!')
-#else:
-#    print('Skipping seed - database has data')
-#"
