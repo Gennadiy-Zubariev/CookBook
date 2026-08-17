1db225396897d1b4fa55fe5d3a60026c8b78ea6f
Gena <genada89@gmail.com>
2026-08-17 23:05:00 +0300

    test adding media

--- build.sh
+++ build.sh
@@ -7,11 +7,21 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-import os
-print('CLOUD_NAME:', os.getenv('CLOUDINARY_CLOUD_NAME'))
-print('API_KEY:', os.getenv('CLOUDINARY_API_KEY', 'NOT SET'))
 from django.conf import settings
-print('STORAGE:', settings.DEFAULT_FILE_STORAGE)
+print('DEFAULT_FILE_STORAGE:', settings.DEFAULT_FILE_STORAGE)
+print('CLOUDINARY_STORAGE:', settings.CLOUDINARY_STORAGE)
+print('MEDIA_URL:', settings.MEDIA_URL)
+
+# Test Cloudinary connection
+import cloudinary
+cloudinary.config(
+    cloud_name=settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+    api_key=settings.CLOUDINARY_STORAGE['API_KEY'],
+    api_secret=settings.CLOUDINARY_STORAGE['API_SECRET'],
+)
+import cloudinary.api
+result = cloudinary.api.ping()
+print('Cloudinary ping:', result)
 "
 
 python manage.py shell -c "
