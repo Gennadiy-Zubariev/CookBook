cfc685442259927a861cc239fd0191ede99b6ce0
Gena <genada89@gmail.com>
2026-08-17 22:39:12 +0300

    return check if database is empty in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+bash
 #!/usr/bin/env bash
 set -o errexit
 
@@ -6,7 +7,11 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.core.management import call_command
-call_command('seed_db', flush=True)
-print('Database re-seeded with Cloudinary!')
+from dishes.models import Recipe
+if not Recipe.objects.exists():
+    from django.core.management import call_command
+    call_command('seed_db')
+    print('Database seeded!')
+else:
+    print('Skipping seed - database has data')
 "
